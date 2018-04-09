3d7f1fda7d88e50cada8fb08dbfe793c73d3fc31
Paul Tiller <pftiller@gmail.com>
2018-04-08 19:44:00 -0500

    starting assignment

--- designs.js
+++ designs.js
@@ -1,7 +1,19 @@
 // Select color input
+var color = $('#colorPicker').val();
+
 // Select size input
+var height = $('#inputHeight').val();
+
+var width = $('#inputWeight').val();
 
 // When size is submitted by the user, call makeGrid()
+
+
+
+
+
+
+
 
 function makeGrid() {
 
