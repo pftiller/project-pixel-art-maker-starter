965fdc74be70734d2bf8b3a95054368f21c27235
Paul Tiller <pftiller@gmail.com>
2018-04-11 16:17:07 -0500

    creating pixelCanvas variable

--- designs.js
+++ designs.js
@@ -1,14 +1,16 @@
 // variables;
 let clicked;
+let pixelCanvas;
 
 // load jquery
 $(document).ready(onLoad);
 
 function onLoad() {
     console.log('JQ loaded');
+    pixelCanvas = $('#pixelCanvas');
     //event listeners
     $('input:submit').on('click', makeGrid);
-    $('#pixelCanvas').on('click','td', function(){
+    pixelCanvas.on('click','td', function(){
             var color = clicked ? '#fff' : $('#colorPicker').val();
             $(this).css('background-color', color);
             clicked = !clicked;
@@ -19,12 +21,12 @@ function onLoad() {
 // functions
 function makeGrid(event) {
     event.preventDefault();
-    $('#pixelCanvas').empty();
+    pixelCanvas.empty();
     let height = $('#inputHeight').val();
     let width = $('#inputWeight').val();
     console.log(height, width);
     for(let i = 0; i < height; ++i) {
-        $('#pixelCanvas').append('<tr id="' + [i] + '">');
+       pixelCanvas.append('<tr id="' + [i] + '">');
         for(let j = 0; j < width; ++j) {
             $('#' + [i]).append('<td></td>');
         }
